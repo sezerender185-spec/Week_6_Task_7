66161f3b270ea3fc80e8132fd6e08bc37cdc5915
sezerender185-spec <sezerender185@gmail.com>
2025-11-04 23:37:22 +0300

    Add files via upload

---
+++ Week_6_Task_7.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+void swap(int* x, int* y) {
+    int temp = *x;
+    * x = * y;
+    *y = temp;
+}
+int main()
+{
+    cout << "Welcome to the swap!" << endl;
+    int x = 7; int y = 6;
+    cout << " Before the Swap: First number is: " << x<< " and Second number is: " << y << endl;
+    swap(&x, &y);
+    cout << "After the Swap: First number is: " << x << " and Second number is: " << y << endl;
+    return 0;
+}
+
